b4f61e1fcdaf3ab992315c39fc8dd60a2874535c
Alex Kiselich <alexander.kiselich@gmail.com>
2023-06-10 20:22:33 -0600

    Updated Coupon Controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,4 +2,28 @@ class CouponsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = Coupon.new(new_coupon_params)
+    if coupon.valid?
+      coupon.save
+      flash.notice = "New Coupon has been created!"
+      redirect_to merchant_coupons_path(merchant)
+    else
+      redirect_to new_merchant_coupon_path(merchant)
+      flash.notice =  "Error: #{coupon.errors.full_messages.to_sentence}"
+    end
+  end
+
+  private
+
+  def new_coupon_params
+    params.permit(:name, :code, :amount, :discount, :merchant_id)
+  end
+
 end
